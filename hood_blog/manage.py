1b30a03fb37be06cfb791cea4bb7d18ecb7d0af4
Sheriff_Hood <h77max@163.com>
2017-12-11 00:03:06 +0800

    update

--- hood_blog/manage.py
+++ hood_blog/manage.py
@@ -2,12 +2,15 @@
 #-*- coding:utf-8 -*-
 
 from flask_script import Manager, Server, Shell
+from flask_migrate import Migrate, MigrateCommand
 import hood_site
 import models
 
 manager = Manager(hood_site.app)
+migrate = Migrate(hood_site.app, models.db)
 
 manager.add_command('server', Server())
+manager.add_command('db', MigrateCommand)
 
 @manager.shell
 def make_shell_context():
