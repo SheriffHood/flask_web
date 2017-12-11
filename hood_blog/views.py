1f2cb60d248c9335a2530d3091e4a60115d7b300
Sheriff_Hood <h77max@163.com>
2017-12-11 23:11:33 +0800

    update

--- hood_blog/views.py
+++ hood_blog/views.py
@@ -14,3 +14,63 @@ def sidebar_data():
     func.count(posts_tags.c.post_id).label('total')).join(posts_tags).group_by(Tag).order_by('total Desc').limit(5).all()
 
     return recent, top_tags
+
+@app.route('/')
+@app.route('/<int:page>')
+def home(page=1):
+    '''view function for home page'''
+
+    posts = Post.query.order_by(
+        Post.publist_date.desc()
+        ).paginate(page, 10)
+
+    recent top_tags = sidebar_data()
+
+    return render_template('home.html',
+                            posts=posts,
+                            recent=recent,
+                            top_tags=top_tags)
+
+@app.route('/post/<string:post_id>')
+def post(post_id):
+    '''view function for post page'''
+
+    post = db.session.query(Post).get_or_404(post_id)
+    tags = post.tags
+    comments = post.comments.order_by(Comment.date.desc()).all()
+    recent, top_tags = sidebar_data
+
+    return render_template('post.html',
+                            post=post,
+                            tags=tags,
+                            comments=comments,
+                            recent=recent,
+                            top_tags=top_tags)
+
+@app.route('/tag/<string:tag_name>')
+def tag(tag_name):
+    '''view function for tag page'''
+
+    tag = db.session.query(Tag).filter_by(name=tag_name).first_or_404()
+    posts = tag.posts.order_by(Post.publish_date.desc()).all()
+    recent, top_tags = sidebar_data()
+
+    return render_template('tag.html',
+                            tag=tag,
+                            posts=posts,
+                            recent=receny,
+                            top_tags=top_tags)
+
+@app.route('/user/<string:username>')
+def user(username):
+    '''view function for user page'''
+
+    user = db.session.query(User).filter_by(username=username).first_or_404()
+    posts = user.posts.order_by(Post.publish_date.desc()).all()
+    recent, top_tags = sidebar_data
+
+    return render_template('user.html',
+                            user=user,
+                            posts=posts,
+                            recent=recent,
+                            top_tags=top_tags)
