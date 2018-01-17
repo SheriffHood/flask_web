c53372da9d5fe5b26d2bd0d3e2edc4e97082e362
Sheriff_Hood <h77max@163.com>
2018-01-17 23:59:54 +0800

    update

---
+++ hoodsite/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#-*- coding:utf-8 -*-
+
+from flask import Flask
+from settings import DevConfig
+
+app = Flask(__name__)
+
+app.config.from_object(DevConfig)
+
+views = __import__('views')
+
+if __name__ == '__main__':
+    app.run()
