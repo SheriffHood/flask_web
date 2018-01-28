d6e2554deb6a6d7b18c1a0da35a80c5506065da1
Sheriff_Hood <h77max@163.com>
2018-01-28 18:16:56 +0800

    add manager blueprint

--- hoodsite/controllers/main.py
+++ hoodsite/controllers/main.py
@@ -5,7 +5,7 @@ from os import path
 from uuid import uuid4
 
 from flask import flash, url_for, redirect, render_template, Blueprint
-from hoodsite.forms import LoginForms, RegisterForm
+from hoodsite.forms import LoginForm, RegisterForm
 from hoodsite.models import User, db
 
 main_blueprint = Blueprint('main', __name__, template_folder=path.join(path.pardir, 'templates', 'main'))
@@ -13,3 +13,35 @@ main_blueprint = Blueprint('main', __name__, template_folder=path.join(path.pard
 @main_blueprint.route('/')
 def index():
     return redirect( url_for('blog.home') )
+
+@main_blueprint.route('/login', methods=['GET', 'POST'])
+def login():
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash("You have logined in", category="success")
+        return redirect(url_for('blog.home'))
+
+    return render_template('login.html', form=form)
+
+@main_blueprint.route('/logout', methods=['GET', 'POST'])
+def logout():
+
+    flash("You have logged out", category="success")
+    return redirect(url_for('blog.home'))
+
+@main_blueprint.route('/register', methods=['GET', 'POST'])
+def register():
+
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        new_user = User(id=str(uuid4()), username=form.username.data, password=form.password.data)
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash("Your user has been created, please login", category="success")
+        return redirect(url_for('main.login'))
+
+    return render_template('register.html', form=form)
